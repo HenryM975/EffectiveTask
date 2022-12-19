dac0a52646bfab8232dc9ddd5797ed2d5fde5098
unknown <henry975@yandex.ru>
2022-12-19 19:24:31 +0300

    ATTest

--- app/src/main/java/com/example/effectivetask/QRActivity.java
+++ app/src/main/java/com/example/effectivetask/QRActivity.java
@@ -1,26 +1,35 @@
+//TESTS
+
 package com.example.effectivetask;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class QRActivity extends AppCompatActivity {
-
+    //0
     AppCompatButton qradd;
     List<View> allEds;
+
+    //1
+    ProgressBar progressbarQR1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qractivity);
+        //0
 
         qradd = findViewById(R.id.qradd);
         allEds = new ArrayList<View>();
@@ -38,8 +47,6 @@ public class QRActivity extends AppCompatActivity {
                 qrlinear.addView(view);//add to page
             }
         });
-
-
         //Delete
         /*Button deleteField = (Button) view.findViewById(R.id.button2);
         deleteField.setOnClickListener(new View.OnClickListener() {
@@ -57,5 +64,37 @@ public class QRActivity extends AppCompatActivity {
         });*/
 
 
+
+
+
+        //1
+        progressbarQR1 = findViewById(R.id.progressbarQR1);
+        //QR1Background QR1 = new QR1Background();
+        //QR1.execute();
+        new QR1Background().execute();
+
+
+    }
+    //1
+    class QR1Background extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressbarQR1.setVisibility(View.VISIBLE);
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            progressbarQR1.setVisibility(View.INVISIBLE);
+        }
     }
 }
